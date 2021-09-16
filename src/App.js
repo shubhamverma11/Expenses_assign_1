1cd0b1aa6ddb5de2969f5cf562318b6a00f05b7d
LAPTOP-JLQTC8T2\shubh <shubham.verma37@gmail.com>
2021-09-16 20:58:32 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Food",
+      amount: 500.0,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "Electricity",
+      amount: 100.0,
+      date: new Date(2021, 8, 15),
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,8 +37,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a>    
+      </header> */}
+      <Expenses items={expenses}></Expenses>
+
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+// const expenseDate = new Date(2021, 2, 28);
+// const expenseTitle = 'Car Insurance';
+// const expenseAmount = 294.67;
+
+function ExpenseItem(props) {
+
+  return (
+    <div className="expense-item">
+        <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
